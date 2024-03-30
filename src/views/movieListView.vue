b65fa9b4b5b908fbfe8e8778e876c24d95884aca
kodasom0302 <dasom589@gmail.com>
2024-03-30 21:06:41 +0900

    영화 수정, 연령 사진 추가, css 수정

--- src/views/movieListView.vue
+++ src/views/movieListView.vue
@@ -12,7 +12,12 @@
         <tbody>
             <tr>
                 <td rowspan="3">1</td>
-                <td class="title" rowspan="3"><img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240222_175%2F1708609058134r4oaq_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2">파묘</td>
+                <td class="title" rowspan="3">
+                    <div>
+                        <img id="moviePoster" src="@/assets/img/파묘.jpg">
+                        <img id="ageImg" src="@/assets/img/15.svg">
+                    </div>파묘
+                </td>
                 <td>09:30~11:45</td>
                 <td><button>29/40</button></td>
             </tr>
@@ -27,7 +32,12 @@
 
             <tr>
                 <td rowspan="3">2</td>
-                <td class="title" rowspan="3"><img src="https://search.pstatic.net/common?type=o&size=176x264&quality=85&direct=true&src=https%3A%2F%2Fs.pstatic.net%2Fmovie.phinf%2F20240327_285%2F1711521875538PWaOx_JPEG%2Fmovie_image.jpg%3Ftype%3Dw640_2">댓글부대</td>
+                <td class="title" rowspan="3">
+                    <div>
+                        <img id="moviePoster" src="@/assets/img/댓글부대.jpg">
+                        <img id="ageImg" src="@/assets/img/15.svg">
+                    </div>댓글부대
+                </td>
                 <td>12:20~14:19</td>
                 <td><button>38/40</button></td>
             </tr>
@@ -42,7 +52,12 @@
 
             <tr>
                 <td rowspan="3">3</td>
-                <td class="title" rowspan="3"><img src="https://search.pstatic.net/common/?src=https%3A%2F%2Fshopping-phinf.pstatic.net%2Fmain_3246458%2F32464584735.20221019114947.jpg&type=w216">극장판 짱구는 못말려: 습격!! 외계인 덩덩이</td>
+                <td class="title" rowspan="3">
+                    <div>
+                        <img id="moviePoster" src="@/assets/img/코난.jpg">
+                        <img id="ageImg" src="@/assets/img/12.svg">
+                    </div>명탐정 코난 - 흑철의 어영
+                </td>
                 <td>07:50~09:50</td>
                 <td><button>18/20</button></td>
             </tr>
@@ -60,14 +75,13 @@
 
 
 <script>
-import "@/assets/movieList.css"
+import "@/assets/css/movieList.css"
 
 export default {
     name: "movieListView",
     components: {},
     data() {
-        return {
-        };
+        return {};
     },
     methods: {},
 };

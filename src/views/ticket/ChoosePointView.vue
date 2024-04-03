19928e54b11a937c1334478be16e0162b41ab671
kodasom0302 <dasom589@gmail.com>
2024-04-03 12:48:53 +0900

    포인트 선택/적립/완료창 수정, 추가

---
+++ src/views/ticket/ChoosePointView.vue
@@ -0,0 +1,68 @@
+<template>
+    <div>
+        <div id="wrap">
+
+            <AppHeader />
+
+            <div id="container" class="clearfix">
+
+                <div id="content">
+
+
+
+
+                    <div id="box-btn">
+                        <h1 id="title">포인트</h1>
+
+                        <router-link id="btnAdd" to="/ticket/pluspoint">
+                            <img id="imgAddAndUse" src="@/assets/img/addPoint.png">
+                            적립
+                        </router-link>
+
+                        <router-link id="btnUse" to="/ticket/pointuse">
+                            <img id="imgAddAndUse" src="@/assets/img/usePoint.png">
+                            사용
+                        </router-link>
+
+                        <router-link id="btnNot" to="/payment/paymentform">
+                            <img id="imgX" src="@/assets/img/X.png">
+                            적립 안함
+                        </router-link>
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+
+            <AppFooter />
+
+        </div>
+    </div>
+</template>
+
+
+<script>
+import "@/assets/css/ChoosePoint.css";
+import AppHeader from "@/components/AppHeader.vue"
+import AppFooter from "@/components/AppFooter.vue"
+
+export default {
+    name: "ChoosePointView",
+    components: {
+        AppFooter,
+        AppHeader,
+    },
+    data() {
+        return {
+            isModalViewed: false,
+        };
+    },
+    methods: {},
+};
+</script>
+
+
+<style></style>
